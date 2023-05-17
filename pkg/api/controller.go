1aa56e7e3a9aa83e1dfb43817a72f6dc7949eb44
Elad Lachmi <elad.lachmi@treeverse.io>
2023-05-17 15:25:34 +0300

    Review fixes

--- pkg/api/controller.go
+++ pkg/api/controller.go
@@ -2505,6 +2505,9 @@ func (c *Controller) UploadObjectPreflight(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	ctx := r.Context()
+	c.LogAction(ctx, "put_object_preflight", r, repository, branch, "")
+
 	writeResponse(w, r, http.StatusNoContent, nil)
 }
 
@@ -2933,6 +2936,9 @@ func (c *Controller) SetGarbageCollectionRulesPreflight(w http.ResponseWriter, r
 		return
 	}
 
+	ctx := r.Context()
+	c.LogAction(ctx, "set_gc_collection_rules_preflight", r, repository, "", "")
+
 	writeResponse(w, r, http.StatusNoContent, nil)
 }
 
@@ -3056,6 +3062,10 @@ func (c *Controller) CreateBranchProtectionRulePreflight(w http.ResponseWriter, 
 	}) {
 		return
 	}
+
+	ctx := r.Context()
+	c.LogAction(ctx, "create_branch_protection_rule_preflight", r, repository, "", "")
+
 	writeResponse(w, r, http.StatusNoContent, nil)
 }
 
